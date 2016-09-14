e5edb3f958a0b009ace717f8698c92a33630eb4f
bovojon <bovojon@gmail.com>
2016-09-14 14:49:11 -0400

    Build Design for News and Events

---
+++ app.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cssApp', ['ui.router', 'ngMaterial'])
+    .config(configFunction)
+    .controller('cssCtrl', cssCtrl);
+
+  /////////////////
+  configFunction.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function configFunction($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+          url: '/',
+          templateUrl: 'home.html',
+          controller: 'cssCtrl',
+          controllerAs: 'vm',
+      });
+
+  }
+
+  /////////////////
+  cssCtrl.$inject = [];
+  function cssCtrl() {
+    var vm = this;
+    vm.message = "Hello World!";
+
+  }
+
+
+
+})();
